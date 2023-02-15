64a0843cdfe1458ed388e44891e9538fd067282e
hadishnr <hadishofinur@gmail.com>
2023-02-15 18:50:29 +0800

    implement game logic

--- script.js
+++ script.js
@@ -8,11 +8,37 @@ document.querySelector('.number').textContent = 10;
 document.querySelector('.guess').value = 20;
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number !';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high bro';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost! Hahaha';
+
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low bro';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost! Hahaha';
+
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
