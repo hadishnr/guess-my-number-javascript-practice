73b6b90c18030fad841209489392819bd62c49dd
hadishnr <hadishofinur@gmail.com>
2023-02-16 09:19:49 +0800

    changed style when player win

--- script.js
+++ script.js
@@ -10,16 +10,24 @@ document.querySelector('.guess').value = 20;
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  //When no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number !';
+
+    //When player win
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    //When guess to high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high bro';
@@ -30,6 +38,8 @@ document.querySelector('.check').addEventListener('click', function () {
 
       document.querySelector('.score').textContent = 0;
     }
+
+    //When guess to low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too Low bro';
